24346b41c87a251c1305d47927609a12ffe664e0
Iakov Gazizov <imcuber@live.ru>
2020-09-23 14:05:25 +0300

    Add fmt all-in one include file

---
+++ IonEngine/include/IonEngine/extern/fmtlib.h
@@ -0,0 +1,11 @@
+#ifndef IONENGINE_EXTERN_FMTLIB_H
+#define IONENGINE_EXTERN_FMTLIB_H
+
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+#include <fmt/os.h>
+#include <fmt/printf.h>
+
+#endif // IONENGINE_EXTERN_FMTLIB_H
